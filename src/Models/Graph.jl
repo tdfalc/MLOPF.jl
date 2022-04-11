dcbf28b37c9e16726bc2d3a938fcf3407ecc6782
tdfalc <falconertd@gmail.com>
2022-04-11 17:41:09 +0100

    Simplify GNN implementation

--- src/Models/Graph.jl
+++ src/Models/Graph.jl
@@ -3,7 +3,10 @@ using GeometricFlux
 using MLOPF
 
 abstract type Graph <: NeuralNetwork end
-struct GraphLayer <: NeuralNetworkLayer end
+
+function model_input(::Type{Graph}, data::Vector{MLOPF.ProcessedSample})
+    return map(x -> FeaturedGraph(x.adjacency_matrix, nf = hcat([x.pd, x.qd]...)'), data)
+end
 
 """
     convolutional_neural_network(layers::Vector{MLOPF.Layer}, drop_out::Float64)
@@ -20,42 +23,24 @@ This function builds a convolutional neural network graph using Flux's Chain typ
 # Outputs
     - `Flux.Chain`: Convolutional neural network.
 """
-function graph_neural_network(layers::Vector; conv = ChebConv, drop_out::Float64 = 0.0, kwargs...)
-    chain = []
-    for layer ∈ layers
-        if isa(layer, MLOPF.GraphLayer)
-            push!(graph, conv(layer.in => layer.out; kwargs...))
-            # Try instance norm
-            # Does batchnorm even work here? As we have a single instance.
-            # push!(chain, x -> FeaturedGraph(x.graph.S, nf=Flux.BatchNorm(layer.out)(x.nf)))
-            push!(chain, x -> FeaturedGraph(x.graph.S, nf = Flux.dropout(x.nf, drop_out)))
-        else
-            push!(chain, x -> vec(x.nf'))
-            push!(chain, Flux.Dense(layer.in, layer.out, layer.act))
-        end
+function graph_neural_network(
+    ::Type{l},
+    size_in::int,
+    size_out::int,
+    num_layers::int;
+    act::Function = Flux.relu,
+    fact::Function = Flux.sigmoid,
+    kwargs...,
+) where {l<:GeometricFlux.AbstractGraphLayer}
+    (num_channels, num_nodes), chain = size_in, []
+    for i ∈ 1:(num_layers)
+        in, out = MLOPF.get_size(num_channels, i - 1), MLOPF.get_size(num_channels, i)
+        push!(chain, l(in => out, act; kwargs...))
+        # TODO: Check batch norm implementation.
+        push!(chain, x -> FeaturedGraph(x.graph.S, nf = Flux.BatchNorm(layer.out)(x.nf)))
+        push!(chain, x -> FeaturedGraph(x.graph.S, nf = Flux.dropout(x.nf, drop_out)))
     end
-    return Flux.Chain(graph...)
+    push!(chain, x -> vec(x.nf'))
+    push!(chain, Flux.Dense(num_nodes * size(index - 1), size_out, fact))
+    return Flux.Chain(chain...)
 end
-
-function model_input(::Type{Graph}, data::Vector{MLOPF.ProcessedSample})
-    return map(x -> FeaturedGraph(x.adjacency_matrix, nf = hcat([x.pd, x.qd]...)'), data)
-end
-
-function graph_layer(index::Int, num_layers::Int, size_in::Tuple, size_out::Int, act, fact)
-    num_channels, num_nodes = size_in
-    size(i) = i == 0 ? num_channels : ceil(Int, num_channels / 4) * 4 * 2^(i - 1)
-    if index <= num_layers
-        return MLOPF.GraphLayer(size(index - 1), size(index), act)
-    end
-    return MLOPF.FullyConnectedLayer(num_nodes * size(index - 1), size_out, fact)
-end
-
-define_layers(
-    ::Type{MLOPF.Graph},
-    size_in::Tuple,
-    size_out::Int,
-    num_layers::Int;
-    act = Flux.relu,
-    fact = Flux.sigmoid,
-) = return map(l -> graph_layer(l, num_layers, size_in, size_out, act, fact), 1:(num_layers+1))
-
